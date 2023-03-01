cbbe60a811b03fb7f7d17b2d66f64206ddcf2cdc
ANJVII <118735231+ANJVII@users.noreply.github.com>
2023-03-01 20:39:52 +0800

    Add files via upload
    - v1

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,41 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/profile")
+    public String get(Principal principal, Model model) {
+        User user = userService.findByEmail(principal.getName());
+        model.addAttribute("profile", user);
+        return "profile";
+    }
+
+    // Login form
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    // Login form with error
+    @RequestMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login";
+    }
+}
